49d0a88e7891596ab64b1e053303ed4a7718c855
vivek1242 <bhosvivek123@gmail.com>
2023-10-12 12:33:24 +0530

    Assignment_4 SetA_1 added

---
+++ src/Assignment_4/SetA/SetA_1.java
@@ -0,0 +1,30 @@
+package Assignment_4.SetA;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SetA_1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login Form");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 120);
+        frame.setLayout(new FlowLayout()); // Using FlowLayout as the layout manager
+
+        // Username Label and TextField
+        JLabel usernameLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField(20);
+
+        // Password Label and PasswordField
+        JLabel passwordLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField(20);
+
+        // Add components to the frame
+        frame.add(usernameLabel);
+        frame.add(usernameField);
+        frame.add(passwordLabel);
+        frame.add(passwordField);
+
+        // Set frame visibility
+        frame.setVisible(true);
+    }
+}
